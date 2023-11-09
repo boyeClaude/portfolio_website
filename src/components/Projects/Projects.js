02e8c4f10d85c0cca1e6bdfa1e3d643e895e5680
Frederic Boye <boye.claude@gmail.com>
2023-11-09 23:33:49 +0000

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => (
     // <GridContainer>
     //   {myProjects.map(({ id, image, title, description, source, tags, visit }) => (
     //     <BlogCard key={id}>
-    //       <Img src={image} />
+    //       <Img src={image} />  
     //       <TitleContent>
     //         <HeaderThree title>{title}</HeaderThree>
     //         <Hr />
