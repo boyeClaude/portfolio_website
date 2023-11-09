5d7c0cca2a6a2d2b007dd01d0846d3080c9df7de
Frederic Boye <boye.claude@gmail.com>
2023-11-09 23:31:16 +0000

    Update Projects.js
    comment grid container to hide showcase projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,42 +2,42 @@ import React from "react";
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from "./ProjectsStyles";
 import { Section, SectionDivider, SectionTitle } from "../../styles/GlobalComponents";
-import {  myProjects } from "../../constants/constants";
+import {  myProjects, projects } from "../../constants/constants";
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Projects</SectionTitle>
-    <GridContainer>
-      {myProjects.map(({ id, image, title, description, source, tags, visit }) => (
-        <BlogCard key={id}>
-          <Img src={image} />
-          <TitleContent>
-            <HeaderThree title>{title}</HeaderThree>
-            <Hr />
-          </TitleContent>
-          <CardInfo>{description}</CardInfo>
-          <br />
-          <div>
-            <TitleContent>Stack</TitleContent>
+    // <GridContainer>
+    //   {myProjects.map(({ id, image, title, description, source, tags, visit }) => (
+    //     <BlogCard key={id}>
+    //       <Img src={image} />
+    //       <TitleContent>
+    //         <HeaderThree title>{title}</HeaderThree>
+    //         <Hr />
+    //       </TitleContent>
+    //       <CardInfo>{description}</CardInfo>
+    //       <br />
+    //       <div>
+    //         <TitleContent>Stack</TitleContent>
 
-            <TagList>
-              {tags.map((tag, i) => (
-                <Tag key={i}>{tag}</Tag>
-              ))}
-            </TagList>
-          </div>
-          <UtilityList>
-            <ExternalLinks href={source} target="_blank">
-              Code
-            </ExternalLinks>
-            <ExternalLinks href={visit} target="_blank">
-              Live
-            </ExternalLinks>
-          </UtilityList>
-        </BlogCard>
-      ))}
-    </GridContainer>
+    //         <TagList>
+    //           {tags.map((tag, i) => (
+    //             <Tag key={i}>{tag}</Tag>
+    //           ))}
+    //         </TagList>
+    //       </div>
+    //       <UtilityList>
+    //         <ExternalLinks href={source} target="_blank">
+    //           Code
+    //         </ExternalLinks>
+    //         <ExternalLinks href={visit} target="_blank">
+    //           Live
+    //         </ExternalLinks>
+    //       </UtilityList>
+    //     </BlogCard>
+    //   ))}
+    // </GridContainer>
   </Section>
 );
 
